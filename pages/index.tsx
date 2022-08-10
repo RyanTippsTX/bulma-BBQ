8ac036eec00e2f31f8828a1b7d5d338cc6f516db
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-08-10 15:38:17 -0500

    debug CSS refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+// import styles from '../styles/Home.module.css';
 import Hero from '../components/hero';
 import Menu from '../components/menu';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>The Tipps BBQ Experience</title>
         <meta name="description" content="Menu for the Tipps BBQ Experience" />
