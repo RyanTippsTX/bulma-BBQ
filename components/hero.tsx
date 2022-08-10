d07e11317cb8bf7135b383c9ea4a6cb7a75fe187
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-08-10 15:28:41 -0500

    refactor Hero and Menu into React components

---
+++ components/hero.tsx
@@ -0,0 +1,21 @@
+export default function Hero() {
+  return (
+    <>
+      <section className="hero">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <p className="title is-4 main-title">- The TIPPS BBQ -</p>
+            <p className="subtitle is-5 main-subtitle">
+              <i className="fa-solid fa-fire-flame-simple flame"></i>
+              <i className="fa-solid fa-2xs fa-fire-flame-simple flame"></i>
+              Experience
+              <i className="fa-solid fa-2xs fa-fire-flame-simple flame"></i>
+              <i className="fa-solid fa-fire-flame-simple flame"></i>
+            </p>
+          </div>
+        </div>
+      </section>
+      <div className="horizontal-rule"></div>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import Hero from '../components/hero';
+import Menu from '../components/menu';
 
 const Home: NextPage = () => {
   return (
@@ -18,7 +20,8 @@ const Home: NextPage = () => {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥩</text></svg>"
         />
       </Head>
-      <h2>Content</h2>
+      <Hero />
+      <Menu />
     </div>
   );
 };
