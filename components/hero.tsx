de278c652e79a1b895d5426da3ca0ac501e2bdbf
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-08-11 02:13:46 -0500

    fix whitespace in Hero SVG flame

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,11 +6,13 @@ export default function Hero() {
           <div className="container has-text-centered">
             <p className="title is-4 main-title">- The TIPPS BBQ -</p>
             <p className="subtitle is-5 main-subtitle">
-              <i className="fa-solid fa-fire-flame-simple flame"></i>
-              <i className="fa-solid fa-2xs fa-fire-flame-simple flame"></i>
-              Experience
-              <i className="fa-solid fa-2xs fa-fire-flame-simple flame"></i>
-              <i className="fa-solid fa-fire-flame-simple flame"></i>
+              <i className="fa-solid fa-fire-flame-simple flame" />
+              &nbsp;
+              <i className="fa-solid fa-2xs fa-fire-flame-simple flame" />
+              &nbsp;Experience&nbsp;
+              <i className="fa-solid fa-2xs fa-fire-flame-simple flame" />
+              &nbsp;
+              <i className="fa-solid fa-fire-flame-simple flame" />
             </p>
           </div>
         </div>
